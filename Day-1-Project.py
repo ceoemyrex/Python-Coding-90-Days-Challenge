938109eb2b4ba30a578ffdc30cb48422f99b85ba
Chiemerie <onuohaemma167@gmail.com>
2025-01-09 00:21:59 +0100

    Day 1 Push

---
+++ Day-1-Project.py
@@ -0,0 +1,14 @@
+#  Project: 
+# - Write a simple Python script that prints "Hello, World!" and the results of basic arithmetic (e.g., 2 + 2, 5 * 3).
+
+print("Hello, world!")  # Prints: Hello, world!
+
+#Basic Arithemetic
+
+# Multiplication
+result_multiplication = 5 * 6
+print(f"5 * 6 = {result_multiplication}")
+
+# Addition
+result_addition = 6 + 1
+print(f"6 + 1 = {result_addition}")
